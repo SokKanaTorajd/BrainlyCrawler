06295665a1fa5afc703f4ea9be467b502621f240
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2021-04-02 16:35:19 +0700

    db connection

--- database.py
+++ database.py
@@ -4,4 +4,8 @@ class dbBrainly():
     client = MongoClient()
     database = 'brainlydb'
 
+    def insert_url(self, collection, url):
+        db  = self.client[self.database]
+        db[collection].insert_one({'url':url})
+
     
