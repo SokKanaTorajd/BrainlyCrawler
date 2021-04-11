8399f39a67b82012ea11bb530d33d7aa58e01846
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2021-04-11 17:51:10 +0700

    create get_all_urls and insert_info functions

--- database.py
+++ database.py
@@ -8,4 +8,13 @@ class dbBrainly():
         db  = self.client[self.database]
         db[collection].insert_one({'url':url})
 
+    def get_all_urls(self, collection):
+        db = self.client[self.database]
+        collection = db['bindo']
+        result = collection.find()
+        return result
+
+    def insert_info(self, collection, data):
+        db = self.client[self.database]
+        db[collection].insert_one(data)
     
